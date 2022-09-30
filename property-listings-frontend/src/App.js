35d9ced40d47ac6311668cd4668c975e645a7ff8
Julien Hnery <jwh_917@yahoo.com>
2022-09-30 08:20:37 -0400

    Created Temporary HeaderBar - temp logo, temp search bar, add button and temporary icon

--- property-listings-frontend/src/App.js
+++ property-listings-frontend/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ 
+
+      <div className="HeaderBar">
+        <div className="HeaderBarGrid">
+          <div style={{"marginTop" : "-95px"}} >
+            <h2 style={{"fontSize" : "100px"}}>🏘</h2>
+          </div>
+
+          <div>
+            <h2>Search Bar</h2>
+            <div className="buttonContainer">
+              
+           <button>Add New Property</button>
+
+            </div>
+          </div>
+
+          <div>
+            <h2>Fake Icon 👤</h2>
+          </div>
+ 
+        </div>
+        {/* chagne hr css */}
+        <hr></hr>
+
+        <br></br>
+        <br></br>
+      </div>
+        
+
+     {/* 1 HeaderBar */}
+        {/* ThreeDHouse */}
+        {/* SearchBarButton */}
+        {/* CreateListingButton */}
+        {/* FakeIcon/Burger */}
+      {/* 2 PropertyListingForm */}
+      {/* 3 SortFilterContainer */}
+        {/* HousingCategoryBar  */}
+        {/* FilterButton */}
+
+      {/* 4 ListingsContainer */}
+
     </div>
   );
 }
