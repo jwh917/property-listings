a8b762570d04ea3925120344400458656ca1b438
Julien Hnery <jwh_917@yahoo.com>
2022-10-01 15:09:29 -0400

    Created HeaderBar.js and updated App.js

--- property-listings-frontend/src/App.js
+++ property-listings-frontend/src/App.js
@@ -1,68 +1,19 @@
 import './App.css';
-import React, { useState } from "react";
+import React from "react";
 
-import PropertyListingForm from "./PropertyListingForm";
-import CategoryBar from './CategoryBar';
+import HeaderBar from './HeaderBar';
 
 
 function App() {
 
-  const [showForm, setShowForm] = useState(false);
-
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
-  }
-
-
   return (
     <div className="App">
 
- 
-
-      <div className="headerBar">
-        <div className="headerBarGrid">
-          
-          <div style={{"marginTop" : "-85px"}} >
-            <h2 style={{"fontSize" : "100px", "border": "1px solid black"}}>🏘</h2>
-          </div>
-
-          <div>
-            <h1>Property Listing 🏘</h1>
-            <div className="search-box">
-              <input type="text" className="input-search" placeholder="🔎"></input>
-            </div>
-            <div className="buttonContainer">
-              
-            {showForm ?<button onClick={handleClick}>Close <br></br> ❌ </button> : <button onClick={handleClick}>Add New Property</button>}
-
-            </div>
-          </div>
-
-          <div>
-
-            <div style={{"border": "1px solid black"}}>  
-                <button id="hamburger-menu">
-                  <nav id="sidebar-menu"></nav>
-                </button>
-            </div>
-            
-          </div>
- 
-        </div>
-
-        {showForm ? <PropertyListingForm /> : <CategoryBar />
-                
-        }
-
-        <hr className="hr"></hr>
+      <HeaderBar />
 
 
-        {/* filter button remains at the bottom of the page as you scroll thru properties  */}
+      {/* filter button remains at the bottom of the page as you scroll thru properties  */}
 
-        <br></br>
-        <br></br>
-      </div>
-        
 
      {/* 1 HeaderBar */}
         {/* ThreeDHouse  */}
