feeda13248e124b28f0367166f1abf410d8d393f
Julien Hnery <jwh_917@yahoo.com>
2022-09-30 22:41:54 -0400

    Updated PropertyListingForm.js and App.css

--- property-listings-frontend/src/PropertyListingForm.js
+++ property-listings-frontend/src/PropertyListingForm.js
@@ -13,6 +13,76 @@ function PropertyListingForm() {
           className="input-text"
         />
 
+        <br></br>
+
+        <input
+          type="text"
+          name="location"
+          placeholder="Location..."
+          className="input-text"
+        />
+
+        <br></br>
+
+        <input
+          type="text"
+          name="images"
+          placeholder="Image URLs..."
+          className="input-text"
+        />
+
+        <br></br>
+
+        <input
+        // number
+          type="text"
+          name="price"
+          placeholder="Price $..."
+          className="input-text"
+        />
+
+        <br></br>
+        <br></br>
+
+
+        <button> Categories </button>
+        <br></br>
+        <br></br>
+
+
+        <button> Types </button>
+        <br></br>
+        <br></br>
+
+
+        <input
+        // number
+          type="text"
+          name="price"
+          placeholder="# of Bedrooms..."
+          className="input-text"
+        />
+        <br></br>
+
+        <input
+        // number
+          type="text"
+          name="price"
+          placeholder="# of Bathrooms..."
+          className="input-text"
+        />
+        <br></br>
+        <br></br>
+
+
+        <input
+          type="submit"
+          name="submit"
+          value="Create New Property"
+          className="submit"
+        />
+
+
       </form>
     </div>
   );
