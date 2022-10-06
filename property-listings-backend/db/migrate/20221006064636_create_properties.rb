85d87e46f46b4d8115446e4b58655ef005ddfb7b
Julien Hnery <jwh_917@yahoo.com>
2022-10-06 03:02:02 -0400

    Created tables - owners and properties

--- property-listings-backend/db/migrate/20221006064636_create_properties.rb
+++ property-listings-backend/db/migrate/20221006064636_create_properties.rb
@@ -1,4 +1,15 @@
 class CreateProperties < ActiveRecord::Migration[6.1]
   def change
+    create_table :properties do |t|
+      t.string :propertyUrl
+      t.string :name
+      t.string :location
+      t.integer :price
+      t.string :category
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.integer :owner_id
+      t.timestamps
+    end
   end
 end
