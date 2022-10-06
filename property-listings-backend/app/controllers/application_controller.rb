b438feca257621ab8c71a0a479508ab980b5b927
Julien Hnery <jwh_917@yahoo.com>
2022-10-06 03:55:31 -0400

    Created routes - full CRUD capability for properties model

--- property-listings-backend/app/controllers/application_controller.rb
+++ property-listings-backend/app/controllers/application_controller.rb
@@ -6,4 +6,37 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/properties' do
+    properties = Property.all
+    properties.to_json
+  end
+
+  post '/properties' do
+    properties = Property.create(
+      name: params[:name],
+      location: params[:location],
+      propertyUrl: params[:propertyUrl],
+      price: params[:price],
+      category: params[:category],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+    )
+    properties.to_json
+  end
+
+  patch '/properties/:id' do
+    properties = Property.find(params[:id])
+    properties.update(
+      bedrooms: params[:bedrooms],
+      bedrooms: params[:bedrooms]
+    )
+    properties.to_json
+  end
+
+  delete '/properties/:id' do
+    properties = Property.find(params[:id])
+    properties.destroy
+    properties.to_json
+  end
+
 end
